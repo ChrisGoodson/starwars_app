27636ed40b306eeed228d288d04a54ee86eb3cac
chrisgoodson <f.chrisgoodson@gmail.com>
2016-07-28 16:41:28 -0500

    added price range

---
+++ app/models/price.rb
@@ -0,0 +1,11 @@
+class Price
+
+  def get_price
+    (49..999).to_a.sample
+  end
+
+  def get_cent
+    (0..9).to_a.sample
+  end
+
+end
--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,11 +1,8 @@
 class Ticket
 
   def get_ticket
-    # nums =* (0..9)
-    # let =* ('a'..'z')
-    # c_let =* ('A'..'Z')
-    list = (0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a
-    # list =[nums,let,c_let]
+
+    list = (0..9).to_a + ("a".."z").to_a + ("A".."Z").to_a + (0..9).to_a + (0..9).to_a + (0..9).to_a + (0..9).to_a + ("a".."z").to_a
     Array.new(15) do
       list.sample
     end.join('')
